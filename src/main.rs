62c0efa7397929cd8e5cf395cb874cc356ccad5b
Jim D'Angelo <jim@funclobile.com>
2016-01-04 18:18:27 -0800

    Add eat function to philosopher

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,18 @@ impl Philosopher {
             name: name.to_string()
         }
     }
+
+    fn eat(&self) {
+        println!("{} done eating.", self.name);
+    }
 }
 
 fn main() {
-    let p1 = Philosopher::new("Judith Butler");
-    let p2 = Philosopher::new("Gilles Deleuze");
-    let p3 = Philosopher::new("Karl Marx");
-    let p4 = Philosopher::new("Emma Goldman");
-    let p5 = Philosopher::new("Michael Foucault");
+    let philosophers = vec![
+        Philosopher::new("Judith Butler"),
+        Philosopher::new("Gilles Deleuze"),
+        Philosopher::new("Karl Marx"),
+        Philosopher::new("Emma Goldman"),
+        Philosopher::new("Michael Foucault")
+    ];
 }
